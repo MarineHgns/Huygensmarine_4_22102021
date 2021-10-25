f93dfd9cdce8c9c954258af1efe32c1d49124906
Marine <marine.huygens@hotmail.fr>
2021-10-25 14:29:44 +0200

    'add  modal close'

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeBtn = document.getElementsByClassName("close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -19,3 +20,9 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 function launchModal() {
   modalbg.style.display = "block";
 }
+
+// Close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+}
+closeBtn[0].addEventListener("click", closeModal);
