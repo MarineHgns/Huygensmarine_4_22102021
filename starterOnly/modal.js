d49d1d360c4c62e84a4f08a89f73f2694bc72dd4
Marine <marine.huygens@hotmail.fr>
2021-10-26 16:34:34 +0200

    'correctif issues last&first Name + email'

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,3 +1,4 @@
+// Topnav bar
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -12,9 +13,7 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const closeBtn = document.getElementsByClassName("close");
-
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+const inputs = document.querySelectorAll('input[type="text"], [type="email"]');
 
 // launch modal form
 function launchModal() {
@@ -26,3 +25,92 @@ function closeModal() {
   modalbg.style.display = "none";
 }
 closeBtn[0].addEventListener("click", closeModal);
+
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+//fonction si erreur
+const errorDisplay = (tag, message, valid) => {
+  const formData = document.querySelector("." + tag + "-formData");
+  const span = document.querySelector("." + tag + "-formData > span");
+
+  if (!valid) {
+    formData.classList.add("error");
+    span.textContent = message;
+  } else {
+    formData.classList.remove("error");
+    span.textContent = message;
+  }
+};
+
+// Form
+inputs.forEach((input) => {
+  input.addEventListener("input", (e) => {
+    switch (e.target.id) {
+      case "first":
+        firstChecker(e.target.value);
+        break;
+      case "last":
+        lastChecker(e.target.value);
+        break;
+      case "email":
+        emailChecker(e.target.value);
+        break;
+      default:
+        nul;
+    }
+  });
+});
+
+// fonction Prénom et Nom: minimum 2 caractères.
+const firstChecker = (value) => {
+  if (value.length < 2) {
+    errorDisplay("first", "Le prénom doit faire plus de 2 caractères");
+    first = null;
+  } else {
+    errorDisplay("first", "", true);
+    first = value;
+  }
+};
+
+const lastChecker = (value) => {
+  if (value.length < 2) {
+    errorDisplay("last", "Le nom doit faire plus de 2 caractères");
+    last = null;
+  } else {
+    errorDisplay("last", "", true);
+    last = value;
+  }
+};
+
+// fonction Email: valide
+const emailChecker = (value) => {
+  if (
+    !value.match(
+      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+    )
+  ) {
+    errorDisplay("email", "L'adresse mail est incorrecte");
+    email = null;
+  } else {
+    errorDisplay("email", "", true);
+    email = value;
+    console.log(value);
+  }
+};
+
+// fonction submit
+const subForm = document.querySelector("form");
+
+subForm.addEventListener("submit", (e) => {
+  if (first && last && email) {
+    const globalData = {
+      first: first,
+      last: last,
+      email: email,
+    };
+  } else {
+    alert("Veuillez remplir correctement les champs");
+  }
+
+  console.log(test);
+});
