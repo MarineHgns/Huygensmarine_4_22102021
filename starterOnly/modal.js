8d171a07a1d30c23cdca47d990705ac430b2e42c
Marine <marine.huygens@hotmail.fr>
2021-11-08 15:33:35 +0100

    "correctif mentorat js"

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -16,9 +16,23 @@ const $signForm = document.querySelector("#signForm");
 const modalBtnThank = document.querySelectorAll("closeThk");
 const modalThank = document.getElementById("modalThank");
 
-// Fonction ouvrir / fermer la modal (inscription) 2 btns
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalBtnThank.forEach((btn) => btn.addEventListener("click", closeModalThank));
 
+$signForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  if (formValid()) {
+    modalDisplay("none");
+    launchThank();
+    $signForm.reset();
+  }
+});
+
+function launchThank() {
+  modalThank.style.display = "block";
+}
+
+// Fonction ouvrir / fermer la modal (inscription) 2 btns
 function modalDisplay(displayStyle) {
   modal.style.display = displayStyle;
 }
@@ -28,43 +42,35 @@ function launchModal() {
 }
 
 // Fermer modal "Thanks"
-modalBtnThank.forEach((btn) => btn.addEventListener("click", closeModalThank));
+
 function closeModalThank() {
   modalThank.style.display = "none";
 }
 
 ///////////////////////////////////////////////////////////////
 /////////////////////////// Form /////////////////////////////
-// Prénom - 2 caractères minimum
-function firstChecker() {
-  const $firstError = document.querySelector(".firstError");
+// Prénom : Nom - 2 caractères minimum
+function nameChecker(classErreur, input) {
+  const $nameError = document.querySelector(classErreur);
 
-  if (document.getElementById("first").value.length < 2) {
-    $firstError.style.display = "block";
+  if (document.getElementById(input).value.length < 2) {
+    $nameError.style.display = "block";
   } else {
-    return firstChecker;
-  }
-}
-// Nom - 2 caractères minimum
-function lastChecker() {
-  const $lastError = document.querySelector(".lastError");
-
-  if (document.getElementById("last").value.length < 2) {
-    $lastError.style.display = "block";
-  } else {
-    return lastChecker;
+    $nameError.style.display = "none";
+    return nameChecker;
   }
 }
 
-// Email - Non null + regEx contrôle
+// Email - Non null + regEx contrôle email valide
 function emailChecker() {
   let regexEmail =
     /^(([^<>()[]\.,;:s@]+(.[^<>()[]\.,;:s@]+)*)|(.+))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/;
   let email = document.getElementById("email");
   const $emailError = document.querySelector(".emailError");
-  if (email.value.trim() == "" || regexEmail.test(email.value) == false) {
+  if (email.value.trim() === "" || regexEmail.test(email.value) === false) {
     $emailError.style.display = "block";
   } else {
+    $emailError.style.display = "none";
     return emailChecker;
   }
 }
@@ -73,10 +79,11 @@ function dateChecker() {
   const date = document.getElementById("date").value;
   const $dateError = document.querySelector(".dateError");
 
-  if (date == null || date == "") {
+  if (date === null || date === "") {
     $dateError.style.display = "block";
     return false;
   } else {
+    $dateError.style.display = "none";
     return dateChecker;
   }
 }
@@ -85,9 +92,10 @@ function dateChecker() {
 function numberChecker() {
   const number = document.getElementById("number").value;
   const $numberError = document.querySelector(".numberError");
-  if (number == "") {
+  if (number === "") {
     $numberError.style.display = "block";
   } else {
+    $numberError.style.display = "none";
     return numberChecker;
   }
 }
@@ -99,6 +107,7 @@ function locationChecker() {
 
   for (let i = 0; i < $locationRadio.length; i++) {
     if ($locationRadio[i].checked) {
+      $locationError.style.display = "none";
       return locationChecker;
     } else {
       $locationError.style.display = "block";
@@ -108,35 +117,34 @@ function locationChecker() {
 
 // Cgv - cochée d'office (peut être décochée) -> doit obligatoirement être cochée
 
-const cgv = document.getElementById("cgvcase");
-const $cgvError = document.querySelector(".cgvError");
 function cgvChecker() {
-  if (cgv.checked == false) {
+  const cgv = document.getElementById("cgvcase");
+  const $cgvError = document.querySelector(".cgvError");
+  if (cgv.checked === false) {
     $cgvError.style.display = "block";
   } else {
+    $cgvError.style.display = "none";
     return cgvChecker;
   }
 }
 
 // Submit du formulaire, si ok ouvre la modal "Thanks" sinon reste sur la page en gardant le contenu du formulaire
-const modalthank = document.querySelector(".bgroundthank");
-const thankBtn = document.querySelectorAll(".buttonthank");
 
-function launchThank() {
-  modalthank.style.display = "block";
-}
-const formValid = () =>
-  firstChecker() &&
-  lastChecker() &&
-  emailChecker() &&
-  dateChecker() &&
-  numberChecker() &&
-  locationChecker() &&
+const formValid = () => {
+  nameChecker(".firstError", "first");
+  nameChecker(".lastError", "last");
+  emailChecker();
+  dateChecker();
+  numberChecker();
+  locationChecker();
   cgvChecker();
-$signForm.addEventListener("submit", function (e) {
-  e.preventDefault();
-  if (formValid()) {
-    modalDisplay("none");
-    launchThank();
-  }
-});
+  const isValid =
+    nameChecker(".firstError", "first") &&
+    nameChecker(".lastError", "last") &&
+    emailChecker() &&
+    dateChecker() &&
+    numberChecker() &&
+    locationChecker() &&
+    cgvChecker();
+  return isValid;
+};
